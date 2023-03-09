d1c9f2f8387891d7e3622a8c6ea2b48b13fc4da4
keeratiburt <125423996+keeratiburt@users.noreply.github.com>
2023-03-09 14:26:55 +0700

    Add files via upload

---
+++ lab 7-1 send to server.ino
@@ -0,0 +1,46 @@
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "SSID";
+const char* pwd = "password";
+
+float esp_temp = 25.5;
+float esp_humid = 79;
+
+const char* serverName = "https://ชื่อเว็บ.000webhostapp.com/data_receive.php";
+String esp_api_key = "1234567899";
+
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, pwd);
+  Serial.println("กำลังเชื่อมต่อ");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("เชื่อมต่อ Wi-Fi แล้ว IP Address คือ: ");
+  Serial.println(WiFi.localIP());
+
+}
+void loop() {
+
+  HTTPClient http;
+  http.begin(serverName);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  // https://iotghia.000webhostapp.com/data_receive.php?api_key_value=1234567899&temp=25.5&humid=70
+  String httpRequestData = "api_key_value=" + esp_api_key + "&temp=" + esp_temp + "&humid=" + esp_humid;
+
+  int httpRequestCode = http.POST(httpRequestData);
+  Serial.print(httpRequestData);
+  http.end();
+
+  delay(10000);
+
+
+}
