03c4027dfeda7b8dca03abff51d74b6c7ec678e9
keeratiburt <125423996+keeratiburt@users.noreply.github.com>
2023-03-10 14:46:06 +0700

    Update con_db.php

--- lab7.2/con_db.php
+++ lab7.2/con_db.php
@@ -1,1 +1,18 @@
+<?php
 
+$servername = "localhost";
+$username = "id20422443_admin";
+$password = "0123456789aA!";
+$dbname = "id20422443_lab07";
+
+// database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error){
+    die("cannot connect to database" . $conn->connect_error);
+}
+else{
+    echo "connected!";
+}
+
+?>
