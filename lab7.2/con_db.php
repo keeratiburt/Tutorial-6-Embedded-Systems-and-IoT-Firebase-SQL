60eee7be57966c030de646f6c9cfdcef457abd70
keeratiburt <125423996+keeratiburt@users.noreply.github.com>
2023-03-16 13:39:33 +0700

    Update con_db.php

--- lab7.2/con_db.php
+++ lab7.2/con_db.php
@@ -1,13 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username = "id20422443_admin";
-$password = "0123456789aA!";
-$dbname = "id20422443_lab07";
+/* นำข้อมูลมาใส่โดยหาได้จาก Database Manager ใน 000webhost*/
+$servername = "localhost";          // คือหาได้จาก DB Host
+$username = "id20422443_admin";     // หาได้จาก DB User
+$password = "0123456789aA!";        // รหัสผ่านตอนสร้าง Database
+$dbname = "id20422443_lab07";       //  หาได้จาก DB Name
 
-// database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// ส่วนนี้จะเป็นการเชื่อมต่อฐานข้อมูล
+$conn = new mysqli($servername, $username, $password, $dbname); // ตัวแปรเก็บค่าคำสั่งในการเชื่อมต่อฐานข้อมูล
 
+
+// สร้างเงื่อนไขเพื่อตรวจสอบการเชื่อมต่อ โดยหากเชื่อมต่อสำเร็จ จะขึ้นข้อความว่า "connected"
 if ($conn->connect_error){
     die("cannot connect to database" . $conn->connect_error);
 }
